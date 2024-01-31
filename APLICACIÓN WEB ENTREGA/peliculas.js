ca32d7762e649ac7275c3a37158c8d322af8e8fc
Ayline <ikaaponce23@lhusurbil.eus>
2024-01-31 13:58:21 +0100

    commit segundo

---
+++ APLICACIÓN WEB ENTREGA/peliculas.js
@@ -0,0 +1,12 @@
+/*  1.- Primer paso crear muchas funciones.
+    2.- Luego intentar leer toda la informacion desde el API de peliculas
+    3.- Intentar usar solo un js
+    4.- Una vez que tenga todas las peliculas con la informacion sacada desde la API o parseo de json 
+    realizar detalles con js, como botones, animaciones o etc.
+    5.- Tambien arreglar detalles como arreglar el menu de movil que funciona pero se repite codigo.
+    6.- Ver sino tambien se puede tomar el mismo codigo en todas las paginas de html que se repiten como en el header/footer.
+    7.- 
+
+
+
+    */ 
